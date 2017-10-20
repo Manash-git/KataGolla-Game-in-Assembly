19df69d2231326f46e2d6a4cee3d11c33fca437c
Manash-git <manashmondalcse@gmail.com>
2017-10-20 19:42:50 +0600

    variable Declared

--- KataGolla.asm
+++ KataGolla.asm
@@ -5,12 +5,24 @@ org 100h
 .stack 100h
 .model small
 .data  
-
+  	
+	Board_Dis_No 		db 	'123456789'   
+  	Board_structure 	db 	10,13   
+  	                    db 	' | | ',10,13  
+  	                    db 	'-----',10,13
+                        db 	' | | ',10,13 
+                        db 	'-----',10,13
+                        db 	' | | ',10,13  
+    
+   Win_Combination		db 	1,2,3,4,5,6,7,8,9                     
+                        db	1,4,7,2,5,8,3,6,9
+                        db 	1,5,9,3,5,7
 .code  
 
 main proc 
 	mov ax,@data
 	mov ds,ax
+	
 	
 	
 	mov ah,4ch
