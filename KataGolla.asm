bd1eecebad0c7a4c2b34825eae33c970b5b34a1e
Manash-git <manashmondalcse@gmail.com>
2017-10-20 19:02:34 +0600

    Template Code Written

--- KataGolla.asm
+++ KataGolla.asm
@@ -1,16 +1,23 @@
-; A simple Tic Tac Toe Game in multiplayer
+; A simple KataGolla  Game 
 
+include 'EMU8086.INC'
 org 100h
+.stack 100h
 .model small
-.stack 100h 
-.data
-.code
-main proc
+.data  
+
+.code  
+
+main proc 
+	mov ax,@data
+	mov ds,ax
 	
 	
+	mov ah,4ch
+	int 21h 
 	
-	
-mov ah,4ch
-int 21h
+	main endp
+
+end main
 
 	
