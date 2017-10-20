4692f3d1d55eb23130d13a6f99dc027019332cb9
Manash-git <manashmondalcse@gmail.com>
2017-10-20 20:51:28 +0600

    SHOWBOARD print level initialize

--- KataGolla.asm
+++ KataGolla.asm
@@ -14,17 +14,59 @@ org 100h
                         db 	'-----',10,13
                         db 	' | | ',10,13  
     
-   Win_Combination		db 	1,2,3,4,5,6,7,8,9                     
+    Win_Combination		db 	1,2,3,4,5,6,7,8,9                     
                         db	1,4,7,2,5,8,3,6,9
                         db 	1,5,9,3,5,7
+	
+	Board_Positinon 	db	1,3,5,11,13,15,21,23,25
+
 .code  
 
 main proc 
 	mov ax,@data
 	mov ds,ax
 	
+	; setting video mode
 	
+;	mov ah,0
+;	mov al,12h
+;	int 10h
 	
+	; Display colouring
+	
+	MOV AH, 06h    ; Scroll up function
+	sub AL, AL     ; Clear entire screen
+	XOR CX, CX     ; Upper left corner CH=row, CL=column
+	MOV DX, 184FH  ; lower right corner DH=row, DL=column 
+	MOV BH, 1Eh    ; YellowOnBlue   
+	INT 10H
+	
+	; Print Greeting
+	
+    printn "" 
+    print "  			  Welcome Folks " 
+    printn "" 
+    printn ""	 
+	printn "  		  --  KataGolla Game  --"
+	
+	call SHOWBOARD
+
+SHOWBOARD proc
+	  
+	mov cx,9	  
+	sub si,si
+	
+	print:
+		
+		 mov al,Board_Position[si]
+		 mov di,al
+		 
+		
+	
+SHOWBOARD endp
+		
+		
+Exit:	
 	mov ah,4ch
 	int 21h 
 	
