05033faf9462f104a94af8ebf2fb8d42b2889f5e
Manash-git <manashmondalcse@gmail.com>
2017-12-05 00:25:01 +0600

    1 space added before board-Print

--- KataGolla.asm
+++ KataGolla.asm
@@ -64,20 +64,34 @@ endm
   	
 	Board_Default db '123456789$'   
 	
-  	Board_Structure DB ' | | ',10,13
-    				DB '-----',10,13
-     				DB ' | | ',10,13
-     				DB '-----',10,13
-      				DB ' | | ',10,13,10,13,'$'  
-    
-			     
+;  	Board_Structure DB ' | | ',10,13
+;    				DB '-----',10,13
+;     				DB ' | | ',10,13
+;     				DB '-----',10,13
+;      				DB ' | | ',10,13,10,13,'$'  
+
 			;   DB '0|2|4'
     		;	DB '-----'
      		;	DB '14|16|18'
      		;	DB '-----'
       		;	DB '28|30|32'   
       				
-    Board_Position db 0,2,4,14,16,18,28,30,32     
+;    Board_Position db 0,2,4,14,16,18,28,30,32  
+      				
+    Board_Structure DB '  | | ',10,13
+    				DB ' -----',10,13
+     				DB '  | | ',10,13
+     				DB ' -----',10,13
+      				DB '  | | ',10,13,10,13,'$'  
+    
+			     
+            ;   DB '012345|7|9'1011
+    		;	DB '1213141516-----'2123
+     		;	DB '242526272829|31|33'3435
+     		;	DB '3637383940-----'4647
+      		;	DB '484950515253|55|57'
+     
+     Board_Position db 1,3,5,17,19,21,33,35,37 
     
 
 .code  
@@ -118,9 +132,9 @@ Menu proc near
     linebreak
     linebreak
 	
-	print "      P :: PLAY" 
+	print "     P :: PLAY" 
 	linebreak
-	print "      A :: ABOUT"  
+	print "     A :: ABOUT"  
 	
 	Menuloop: 
 	linebreak
